4dd5428f0beb0934a00c71fc5be2e48592241631
rafaella-nunes <faellanunes01@gmail.com>
2023-09-17 20:52:58 -0300

    fix: connects to internet api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from "react";
 import CardList from './CardList';
 import Searchbox from './Searchbox';
-import { robots } from "./robots";
+//import { robots } from "./robots";
 import './App.css';
-
-
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
@@ -23,14 +27,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        console.log(filteredRobots);
-        return(
-            <div className="tc">
-            <h1 className="f1">Robofriends</h1>
-            <Searchbox  searchChange={this.onSearchChange}/>
-            <CardList robots = {filteredRobots}/>
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>No robots here!</h1>
+        }else{
+            return(
+                <div className="tc">
+                <h1 className="f1">Robofriends</h1>
+                <Searchbox  searchChange={this.onSearchChange}/>
+                <CardList robots = {filteredRobots}/>
+                </div>
+            );
+        }
     }
     
 }
